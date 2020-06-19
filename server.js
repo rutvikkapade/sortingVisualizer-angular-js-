2fbd7e3010b093b9ddb9cba3c2b81e3b31a47788
Rutvik Kapade <rutvik609@gmail.com>
2020-06-19 14:49:57 +0530

    'oka16'

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path =require('path');
 const app =express();
-app.use(express.static(__dirname+'/dist/project'));
+app.use(express.static(__dirname+'/dist/myapp'));
 app.get('/*',function(req,res){
     res.sendfile(path.join(__dirname+'/dist/project/index.html'));
 });
