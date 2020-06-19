bf4939cafe1bb0716256bd469867613afccf03d7
Rutvik Kapade <rutvik609@gmail.com>
2020-06-19 14:25:34 +0530

    'oka15'

--- server.js
+++ server.js
@@ -5,4 +5,4 @@ app.use(express.static(__dirname+'/dist/project'));
 app.get('/*',function(req,res){
     res.sendfile(path.join(__dirname+'/dist/project/index.html'));
 });
-app.listen(process.env.PORT||4200);
+app.listen(process.env.PORT||8080);
